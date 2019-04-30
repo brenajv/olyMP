d7c04e9edf6f44285157738b049ed5dd53e04ff6
brenajv <50145404+brenajv@users.noreply.github.com>
2019-04-30 17:15:33 +0200

    Create olymp.user.js

---
+++ olymp.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        olyMP
+// @author      Olympe
+// @version     1.2
+// @downloadURL https://github.com/brenajv/olyMP/raw/master/olymp.user.js
+// @updateURL   https://github.com/brenajv/olyMP/raw/master/olymp.user.js
+// @licence     MIT
+// @description Bot MP
+// @grant       none
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require     script.js
+// @match       *://www.jeuxvideo.com/*
+// @run-at      document-end
+// ==/UserScript==
